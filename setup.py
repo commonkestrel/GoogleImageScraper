663d37a28bc2f464ed2b1e5719627508e963c2b3
Jibble330 <87543311+Jibble330@users.noreply.github.com>
2022-03-27 22:12:45 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+README = (this_directory / "README.md").read_text()
 
 setup(
     name='GoogleImageScraper',
-    packages=find_packages(include=['GoogleImageScraper']),
-    version='1.0.1',
-    description='A package used for scraping results from Google Images and either getting the URLs or downloading the images to the local machine',
+    packages=find_packages(exclude=("tests")),
+    version='1.0.2',
+    description='This is a library for retrieving urls and downloading images from Google Images.',
     author='Jibble',
     license='MIT',
-    install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
-    python_requires='>=3',
+    python_requires='>= 3',
     test_suite='tests',
+    long_description=README,
+    long_description_content_type='text/markdown'
 )
